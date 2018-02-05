e0d113a6ee697e6b81bac5bab0acbe2a0b1a662a
Dylan-Company <445412776@qq.com>
2018-02-04 22:12:17 -0800

    Practice 5

---
+++ 5.c
@@ -0,0 +1,18 @@
+/*输出9*9乘法表*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int i, j;
+	for(i = 1; i <= 9; i++)
+	{
+		for(j = 1; j <= 9; j++)
+		{
+			printf("%d*%d = %-2d\t", i, j, i*j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
